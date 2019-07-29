cb78ce1cedf6b3b26c58d9558788beadc68b5ad0
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-07-29 20:30:01 +0000

    additional revisions of CLI #menu method

--- movie_times/lib/movie_times.rb
+++ movie_times/lib/movie_times.rb
@@ -1,2 +1,8 @@
 require_relative "./movie_times/version"
+
+module MovieTimes
+  #class Error < StandardError; end
+  # Your code goes here...
+end
+
 require_relative './movie_times/cli'
--- movie_times/lib/movie_times/cli.rb
+++ movie_times/lib/movie_times/cli.rb
@@ -3,38 +3,34 @@ class MovieTimes::CLI
   def call 
     list_movies
     menu
-    goodbye
+    #goodbye
   end 
   
   def list_movies
-    # here doc - http://blog.jayfields.com/2006/12/ruby-multiline-strings
+     # here doc
     puts "Coming Soon to a Theatre Near You:"
-    puts #<<–DOC.gsub /^\s*/, ''
-    puts  "1.  Fast & Furious - Hobbs & Shaw - 2019"
-    puts  "2.  Dora and the Lost City of Gold - 2019"
-    #DOC  
+    #puts <<-DOC.gsub /^\s*/, ''
+    puts "1. Fast & Furious - Hobbs & Shaw - 2019" 
+    puts "2. Dora and the Lost City of Gold - 2019" 
+      #DOC 
   end 
   
   def menu
-    puts "Enter the number of the movie you'd like to see or type list to see movie times again or type exit:"
     input = nil 
     while input != "exit"
+      puts "Enter the number of the movie you'd like to see or type exit"
       input = gets.strip.downcase
       case input
       when "1"
-        puts "More info on movie 1..."
+        puts "Showtimes & Tickets for movie 1. Fast & Furious - Hobbs & Shaw - 2019"
       when "2"
-        puts "More info on movie 2..."
-      when "list"
-        list_movies
-      else
-        puts "Not sure what you'd like to see, type list or exit."
+        puts "Showtimes & Tickets for movie 2. Dora and the Lost City of Gold - 2019"
       end 
-    end 
+    end
   end 
   
-  def goodbye
-    puts "See you tomorrow for more movie releases!!"
+  def goodbye 
+    puts "See you next time for more movie premiere updates!!!"
   end 
    
 end 
