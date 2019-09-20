7874f22dcfd8af30a9c27b4509bd8b9087d4f4d9
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-09-20 17:28:47 +0000

    deleted all files and folders

--- movies_nearby/lib/movies_nearby.rb
+++
@@ -1,6 +0,0 @@
-require "movies_nearby/version"
-
-module MoviesNearby
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- movies_nearby/lib/movies_nearby/version.rb
+++
@@ -1,3 +0,0 @@
-module MoviesNearby
-  VERSION = "0.1.0"
-end
